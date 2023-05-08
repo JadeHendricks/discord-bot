a567db2bb017ec1b130a1d106406a97c2abc9a65
Jade <hendricksjade@gmail.com>
2023-05-08 15:07:17 +0200

    add(@mentions): adding in basic @ metions into the onMessageCreate handler.

--- src/events/onMessageCreate.js
+++ src/events/onMessageCreate.js
@@ -1,9 +1,28 @@
+require('dotenv').config();
+
 module.exports = (client) => {
     client.on("messageCreate", (message) => {
-        if (message.author.bot) return;
-        // if (message.content === "BOT") return message.reply("Hey");
-        // if (message.content === "What's up homie?") return message.reply("Hahaha nothing much dude, just chilling in your mothers bed for the night.");
-        // if (message.content === "...") return message.reply("EZ lol.");
-        if (message.content.toLowerCase().includes("dragonic") || message.content.toLowerCase().includes("exavia")) return message.reply("Dragonic is taking a ruk. Please try again later.");
+
+        const mentionedBot = message.mentions.members.has(process.env.CLIENT_ID);
+        const isBot = message.author.bot;
+
+        if (isBot) return;
+        if (mentionedBot) {
+            handleBotAtResponses(message);
+            return;
+        } else {
+            if (message.content.toLowerCase().includes("dragonic") || message.content.toLowerCase().includes("exavia")) {
+                return message.reply("Dragonic is taking a ruk. Please try again later.");
+            }
+        }
     });
+}
+
+function handleBotAtResponses(message) {
+    const hasNoContent = message.content.endsWith(">");
+    if (hasNoContent) {
+        message.reply(`Hello <@${message.author.id}>, what can I help you with?`);
+    } else {
+        message.reply(`I'm current in Development please try again later.`);
+    }
 }
